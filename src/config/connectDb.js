3e79813d2c66430c858b271c077ed13a0f6235a3
Chu Toan <109196305+chutoan99@users.noreply.github.com>
2023-01-10 23:44:47 +0700

    ooooooo

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("shopee", "root", null, {
-  host: "localhost",
-  dialect: "mysql",
-  port: "3306",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "shopee",
+  process.env.DB_USERNAME || "root",
+  process.env.DB_PASSWORD || null,
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: process.env.DB_DIALECT || "mysql",
+    port: process.env.DB_PORT || "3306",
+    logging: false,
+  }
+);
 
 const connectDb = async () => {
   try {
